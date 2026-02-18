62cfec7a87f9f4b8b19732094ae89456f597653e
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-02-18 15:14:33 +0600

    Ongoing Code

---
+++ script/CashOut.js
@@ -0,0 +1,94 @@
+
+
+document.getElementById("CashOut-btn").addEventListener("click",function(){
+
+    const cashOutNumber = getValueFormInput("CashOut-number");
+if(cashOutNumber.length !=11){
+alert("Invalid Agent number")
+return;
+}
+
+
+    const cashOutAmount = getValueFormInput("CashOut-amount")
+
+    const currentBalance = getBalance()
+
+
+const newBalance = currentBalance - Number(cashOutAmount);
+console.log(newBalance);
+
+if(newBalance < 0){
+    alert("Invalid Amount")
+    return;
+}
+
+const pin = getValueFormInput("CashOut-pin");
+
+if(pin === "2350"){
+    alert("cashOut Successfull");
+    setBalance(newBalance);
+    // console.log("newBalance",newBalance);
+}
+else{
+    alert("Invalid Pin")
+    return;
+}
+
+
+
+
+})
+
+
+
+
+
+
+
+// document.getElementById("CashOut-btn").addEventListener("click",function(){
+
+//     const cashOutNumberInput = document.getElementById("CashOut-number");
+//     const cashOutNumber = cashOutNumberInput.value;
+//     console.log(cashOutNumber);
+
+//     if(cashOutNumber.length !=11){
+//         alert("Invalid Agent number")
+//         return;
+//     }
+
+
+// const cashOutAmountInput = document.getElementById("CashOut-amount");
+// const cashOutAmount = cashOutAmountInput.value;
+// console.log(cashOutAmount);
+
+// const balanceElement = document.getElementById("balance");
+// const balance = balanceElement.innerText;
+// console.log(balance);
+
+// const newBalance = Number(balance) - Number(cashOutAmount);
+
+// if(newBalance < 0){
+//     alert("Invalid Amount")
+//     return;
+// }
+
+
+// const cashOutPinInput = document.getElementById("CashOut-pin");
+// const pin = cashOutPinInput.value;
+// console.log(pin);
+
+// if(pin === "2350"){
+//     alert("cashOut Successfull");
+//     console.log("newBalance",newBalance);
+//     balanceElement.innerText = newBalance;
+
+// }
+// else{
+//     alert("Invalid Pin")
+//     return;
+// }
+
+
+
+
+// })
---
+++ script/login.js
@@ -0,0 +1,20 @@
+
+document.getElementById("login-btn").addEventListener("click",function(){
+
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value
+    // console.log(contactNumber);
+
+    const pinInput = document.getElementById("input-pin");
+    const pin = pinInput.value;
+    // console.log(pin);
+
+    if(contactNumber === "01342179060" && pin === "2350"){
+        alert("Login Success")
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Login Failed")
+        return;
+    }
+})
